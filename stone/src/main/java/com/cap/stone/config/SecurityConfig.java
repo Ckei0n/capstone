85c8d5fc0d2cfddafc4c0ebcd262d9d54448135f
K-On <keion.chan@innoedgelabs.com>
2025-08-12 09:04:46 +0800

    upgrade to keycloak 25

--- stone/src/main/java/com/cap/stone/config/SecurityConfig.java
+++ stone/src/main/java/com/cap/stone/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/assets/**", "/static/**", "/favicon.ico", "/api/csrf", "api/auth/verify").permitAll()
+                .requestMatchers("/assets/**", "/static/**", "/favicon.ico", "/api/csrf").permitAll()
                 .requestMatchers("/opensearch/**").hasAuthority("admin")
                 .anyRequest().authenticated()
             )
